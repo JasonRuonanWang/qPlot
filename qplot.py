7a896bcfe8605f7bf65b4c57a2a93feb9bde3b34
Jason Wang <jason.ruonan.wang@gmail.com>
2020-12-01 14:55:26 -0500

    added qplot.py

---
+++ qplot.py
@@ -0,0 +1,35 @@
+import matplotlib
+
+
+def plot(d, t=""):
+    l = 0
+    for p in d:
+        l = len(p)
+        break
+
+    probabilities = []
+    binaries = []
+    decimals = []
+    shots = 0
+
+    for i in range(2**l):
+        b = format(i,"0{0}b".format(l))
+        v = d.get(b)
+        if v == None:
+            probabilities.append(0)
+        else:
+            probabilities.append(v)
+            shots = shots + v
+        binaries.append(b)
+        decimals.append(i)
+
+    title = "Probability distribution of {0} shots".format(shots)
+    title = title + "\n" + t
+    matplotlib.pyplot.plot(decimals, probabilities)
+    matplotlib.pyplot.xticks(decimals, binaries, rotation='vertical', fontsize=1.5)
+    matplotlib.pyplot.title(title)
+    matplotlib.pyplot.savefig("distribution.eps")
+
+
+
+
